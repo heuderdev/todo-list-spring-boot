ef31c9f682c78f28f6abbf841440993e0591085a
heuderdev <heuderdev@gmail.com>
2023-10-16 19:41:42 -0300

    create logs system

--- src/main/java/br/com/heuderdev/todolist/TodolistApplication.java
+++ src/main/java/br/com/heuderdev/todolist/TodolistApplication.java
@@ -1,13 +1,18 @@
 package br.com.heuderdev.todolist;
 
+import org.apache.logging.log4j.LogManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodolistApplication {
-
+public class TodolistApplication  {
+	private static final Logger logger = LoggerFactory.getLogger(TodolistApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
+		logger.info("servidor foi iniciado com sucesso.");
 	}
+
 
 }
--- src/main/java/br/com/heuderdev/todolist/controllers/TaskController.java
+++ src/main/java/br/com/heuderdev/todolist/controllers/TaskController.java
@@ -2,12 +2,15 @@ package br.com.heuderdev.todolist.controllers;
 
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
+import br.com.heuderdev.todolist.TodolistApplication;
 import br.com.heuderdev.todolist.models.TaskModel;
 import br.com.heuderdev.todolist.models.UserModel;
 import br.com.heuderdev.todolist.repositories.ITaskRepository;
 import br.com.heuderdev.todolist.utils.Utils;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,27 +21,28 @@ import java.time.LocalDateTime;
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
+
+    private static final Logger logger = LoggerFactory.getLogger(TodolistApplication.class);
     @Autowired
     private ITaskRepository repository;
 
     @PostMapping
     public ResponseEntity create(@Valid @RequestBody TaskModel taskModel, HttpServletRequest request) {
 
-//        if (taskModel.getTitle().length() > 50) {
-//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{O titulo tem mais que 50 caracteres}");
-//        }
-
         var user_id = request.getAttribute("user_id");
 
         taskModel.setUserId((Long) user_id);
 
+
         var currentDate = LocalDateTime.now();
 
         if(taskModel.getStart_at().isAfter(taskModel.getEnd_at())) {
+            logger.error("A data de inicio deve ser menor que a data de termino.");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de inicio deve ser menor que a data de termino.");
         }
 
         var taskCreated = this.repository.save(taskModel);
+        logger.info("A task: "+ taskModel.getTitle()+" foi criada com sucesso!");
         return ResponseEntity.status(HttpStatus.CREATED).body(taskCreated);
     }
 
